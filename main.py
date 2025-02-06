485b2c4bb1a5b156ea1480a9a98274614a39f5de
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-02-07 01:44:36 +0700

    Исправил обнаружение процесса DD2

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ def start_application() -> None:
     """ожидание запуска DD2 и подлючение к Discord rpc"""
     dd2_status = False
     while dd2_status is False:
-        logger.info("cant find dd2 process, check again after 60 sec")
-        time.sleep(60)
         update_dd2_status = check_dd2_run_in_start()
         if update_dd2_status is True:
             dd2_status = True
+        else:
+            logger.info("cant find dd2 process, check again after 60 sec")
+            time.sleep(60)
     logger.info("dd2 started, wait 30 sec")
     time.sleep(30)
     rpc_service.connect_to_discord_rpc()
