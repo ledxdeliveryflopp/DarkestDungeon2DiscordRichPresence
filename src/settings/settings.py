e0db0469fdabc2c7ddedee9dfd1d1903790e9079
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-02-09 00:40:40 +0700

    Обновил настройки
    1) Добавил смену с русского языка на английский перед запуском
    2) Добавил функции возврата пути лога и языка
    3) Добавил функцию инициализации языка и пути к логу перед запуском(что-бы не открывать каждый раз файл настроек)

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -8,18 +8,37 @@ from loguru import logger
 class GameSettings:
     """Настройки игры"""
     global_game_mode: str = "main"
+    presence_lang: str = "en"
+    log_path: str = None
 
-    @staticmethod
     @logger.catch
-    def get_log_path() -> Any | None:
-        """получение пути к лог файлу из json"""
+    def set_log_path(self) -> Any | None:
+        """Установка пути к лог файлу из json"""
         try:
             with open("settings.json", mode="rb") as settings:
                 json_settings = json.load(settings)
-            return json_settings
+            log_path = json_settings.get("path")
+            self.log_path = log_path
         except Exception as exc:
-            logger.error(f"error while get settings from json: {exc}")
+            logger.error(f"error while get log path from json: {exc}")
             return None
+
+    @logger.catch
+    def set_presence_lang(self) -> None:
+        try:
+            with open("settings.json", mode="rb") as settings:
+                json_settings: dict = json.load(settings)
+            settings_lang = json_settings.get("lang")
+            if settings_lang == "ru" or settings_lang == "en":
+                self.presence_lang = settings_lang
+                logger.info(f"set {settings_lang} lang code")
+        except Exception as exc:
+            logger.error(f"error while get lang from json: {exc}")
+            return None
+
+    @property
+    def get_log_path(self) -> str:
+        return self.log_path
 
     @staticmethod
     @logger.catch
@@ -44,6 +63,17 @@ class GameSettings:
         """установка режима игры"""
         logger.info(f"update global game mode, state - {state}, mode - {mode}")
         self.global_game_mode = mode
+
+    @property
+    def get_presence_lang(self) -> str:
+        """получение режима игры"""
+        return self.presence_lang
+
+    def set_settings(self) -> None:
+        """Установка первоначальных настроек"""
+        self.set_log_path()
+        self.set_presence_lang()
+        logger.info(f"Settings inited, log: {self.log_path}, lang: {self.presence_lang}")
 
 
 game_settings = GameSettings()
