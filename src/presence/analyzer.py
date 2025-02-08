5ce43acef873e203154c7998aae03287e317d863
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-02-09 00:39:23 +0700

    Убрал поиск читаемого названия из функций поиска технических названий

--- src/presence/analyzer.py
+++ src/presence/analyzer.py
@@ -2,7 +2,6 @@ import time
 
 from loguru import logger
 
-from src.presence.const import EnemyConst, LocationConst
 from src.settings.settings import game_settings
 
 
@@ -13,9 +12,9 @@ class LogAnalyzer:
     @logger.catch
     def __get_log_data() -> str:
         """Чтение лога"""
-        json_log_path = game_settings.get_log_path()
+        log_path = game_settings.get_log_path
         try:
-            if not json_log_path:
+            if not log_path:
                 system_user = game_settings.get_system_user_name()
                 logger.info(f"read log with system user: {system_user}")
                 with open(rf'C:\Users\{system_user}\AppData\LocalLow\RedHook\Darkest Dungeon II\Player.log',
@@ -23,7 +22,6 @@ class LogAnalyzer:
                     data = dd2_log_file.read()
                 return data
             else:
-                log_path = json_log_path.get("path")
                 with open(rf'{log_path}', mode="r", encoding='utf-8') as dd2_log_file:
                     data = dd2_log_file.read()
                 return data
@@ -45,7 +43,7 @@ class LogAnalyzer:
 
     @logger.catch
     def get_game_state(self) -> str:
-        """Получения состояния игры"""
+        """Получение состояния игры"""
         log_data = self.__get_log_data()
         log_split = log_data.split("\n")
         game_state_list = [string for string in log_split if
@@ -65,11 +63,9 @@ class LogAnalyzer:
             return None
         locations_len = len(locations_list_embark) - 1
         location_code = locations_list_embark[locations_len].split(" ")[5]
-        embark_location_data = LocationConst.locations_embark.get(location_code)
-        if embark_location_data:
-            return embark_location_data
+        if location_code:
+            return location_code
         else:
-            logger.info(f"cant find location in embark dict, embark code: {location_code}")
             return None
 
     @staticmethod
@@ -82,11 +78,9 @@ class LogAnalyzer:
             return None
         locations_len = len(locations_list_fog) - 1
         location_code = locations_list_fog[locations_len].split(" ")[9]
-        fog_location_data = LocationConst.locations_fog.get(location_code)
-        if fog_location_data:
-            return fog_location_data
+        if location_code:
+            return location_code
         else:
-            logger.info(f"cant find location in fog dict, fog code: {location_code}")
             return None
 
     @logger.catch
@@ -105,7 +99,7 @@ class LogAnalyzer:
                 return None
 
     @logger.catch
-    def get_enemy(self) -> dict | None:
+    def get_enemy(self) -> str | None:
         """Получение типа врагов и их код"""
         log_data = self.__get_log_data()
         log_split = log_data.split("\n")
@@ -115,11 +109,9 @@ class LogAnalyzer:
             return None
         enemy_len = len(enemy_list) - 1
         enemy_code = enemy_list[enemy_len].split(" ")[5]
-        enemy_data = EnemyConst.enemy.get(enemy_code)
-        if enemy_data:
-            return enemy_data
+        if enemy_code:
+            return enemy_code
         else:
-            logger.info(f"cant find enemy in enemy dict, enemy code: {enemy_code}")
             return None
 
 
